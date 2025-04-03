76475e03ab7b6bfd1319fbb79aaefb8aa5ed2769
Tuomo Kriikkula <tuokri@tuta.io>
2025-04-03 23:49:59 +0300

    More basic project initialization

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <dpp/dpp.h>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+int main()
+{
+    return EXIT_SUCCESS;
+}
