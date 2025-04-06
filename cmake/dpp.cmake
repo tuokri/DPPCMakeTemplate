ed31f09e13ee65c251b0e40815096c48f2337940
Tuomo Kriikkula <tuokri@tuta.io>
2025-04-06 11:50:20 +0300

    Silence warnings from dpp

--- cmake/dpp.cmake
+++ cmake/dpp.cmake
@@ -1,6 +1,5 @@
 # These options are used to customize how D++ is built.
 # Check submodules\dpp\CMakeLists.txt for documentation.
-
 set(DPP_INSTALL OFF)
 set(BUILD_VOICE_SUPPORT ON)
 set(RUN_LDCONFIG OFF)
@@ -13,3 +12,15 @@ set(DPP_USE_PCH ON)
 set(BUILD_SHARED_LIBS ON)
 
 add_subdirectory("${CMAKE_SOURCE_DIR}/submodules/dpp")
+
+# Silence compiler warnings from D++ as we have no control over them.
+# If you would like to see them, remove the following block of code.
+get_target_property(DPP_INCLUDE_DIRS dpp INTERFACE_INCLUDE_DIRECTORIES)
+set_property(
+    TARGET
+    dpp
+    APPEND
+    PROPERTY
+    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
+    ${DPP_INCLUDE_DIRS}
+)
